39ada7445cc0f6d1b61782b4598c678ca24ed599
sujit bhoir <sujitbhoir004@gmail.com>
2024-11-21 22:06:55 +0530

    added setting page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,9 +9,11 @@ import NavBar from "./components/NavBar";
 import { useAuthStore } from "./stores/useAuthStore";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./stores/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const  {theme} = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -27,7 +29,7 @@ const App = () => {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <NavBar />
 
       <Routes>
