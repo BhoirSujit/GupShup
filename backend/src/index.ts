49da36305a1e6fb47c3de26dcdbb601debd10e1c
sujit bhoir <sujitbhoir004@gmail.com>
2024-11-22 19:13:41 +0530

    okokokok

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -47,12 +47,13 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-if (valide.NODE_ENV == "development") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", ((req, res) => {
+
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
-  }))
+  });
 }
 
 server.listen(PORT, () => {
