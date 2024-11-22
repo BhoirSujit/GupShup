536720dae9698d9656eeab690f7f20856ec01a66
sujit bhoir <sujitbhoir004@gmail.com>
2024-11-22 19:27:59 +0530

    hope 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -48,11 +48,12 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  console.log("front end path : ",path.join(__dirname, "../frontend/dist"));
 
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });  
 }
 
